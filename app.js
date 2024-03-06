c4b4e311c640ceb524f889bbb4937888a38b1a57
Harshini-3-1 <157867707+Harshini-3-1@users.noreply.github.com>
2024-03-06 21:10:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,181 +1,81 @@
-/*
- *  Created a Table with name todo in the todoApplication.db file using the CLI.
- */
-
-const express = require("express");
-const { open } = require("sqlite");
-const sqlite3 = require("sqlite3");
-const path = require("path");
-
-const databasePath = path.join(__dirname, "todoApplication.db");
+const express = require('express');
+const bodyParser = require('body-parser');
+const User = require('./models/User');
+const Recipe = require('./models/Recipe');
+const db = require('./db');
 
 const app = express();
+app.use(bodyParser.json());
 
-app.use(express.json());
-
-let database = null;
-
-const initializeDbAndServer = async () => {
-  try {
-    database = await open({
-      filename: databasePath,
-      driver: sqlite3.Database,
-    });
-
-    app.listen(3001, () =>
-      console.log("Server Running at http://localhost:3000/")
-    );
-  } catch (error) {
-    console.log(`DB Error: ${error.message}`);
-    process.exit(1);
-  }
-};
-
-initializeDbAndServer();
-
-const hasPriorityAndStatusProperties = (requestQuery) => {
-  return (
-    requestQuery.priority !== undefined && requestQuery.status !== undefined
-  );
-};
-
-const hasPriorityProperty = (requestQuery) => {
-  return requestQuery.priority !== undefined;
-};
-
-const hasStatusProperty = (requestQuery) => {
-  return requestQuery.status !== undefined;
-};
-app.get("/todos/", async (request, response) => {
-  let data = null;
-  let getTodosQuery = "";
-  const { search_q = "", priority, status } = request.query;
-
-  switch (true) {
-    case hasPriorityAndStatusProperties(request.query):
-      getTodosQuery = `
-      SELECT
-        *
-      FROM
-        todo 
-      WHERE
-        todo LIKE '%${search_q}%'
-        AND status = '${status}'
-        AND priority = '${priority}';`;
-      break;
-    case hasPriorityProperty(request.query):
-      getTodosQuery = `
-      SELECT
-        *
-      FROM
-        todo 
-      WHERE
-        todo LIKE '%${search_q}%'
-        AND priority = '${priority}';`;
-      break;
-    case hasStatusProperty(request.query):
-      getTodosQuery = `
-      SELECT
-        *
-      FROM
-        todo 
-      WHERE
-        todo LIKE '%${search_q}%'
-        AND status = '${status}';`;
-      break;
-    default:
-      getTodosQuery = `
-      SELECT
-        *
-      FROM
-        todo 
-      WHERE
-        todo LIKE '%${search_q}%';`;
-  }
-
-  data = await database.all(getTodosQuery);
-  response.send(data);
+// User routes
+app.post('/register', async (req, res) => {
+    try {
+        const user = new User(req.body);
+        await user.save();
+        const token = user.generateAuthToken();
+        res.status(201).send({ user, token });
+    } catch (error) {
+        res.status(400).send({ error: error.message });
+    }
 });
 
-app.get("/todos/:todoId/", async (request, response) => {
-  const { todoId } = request.params;
-
-  const getTodoQuery = `
-    SELECT
-      *
-    FROM
-      todo
-    WHERE
-      id = ${todoId};`;
-  const todo = await database.get(getTodoQuery);
-  response.send(todo);
+app.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findByCredentials(username, password);
+        const token = user.generateAuthToken();
+        res.send({ user, token });
+    } catch (error) {
+        res.status(400).send({ error: error.message });
+    }
 });
 
-app.post("/todos/", async (request, response) => {
-  const { id, todo, priority, status } = request.body;
-  const postTodoQuery = `
-  INSERT INTO
-    todo (id, todo, priority, status)
-  VALUES
-    (${id}, '${todo}', '${priority}', '${status}');`;
-  await database.run(postTodoQuery);
-  response.send("Todo Successfully Added");
+// Recipe routes
+app.get('/recipes', async (req, res) => {
+    try {
+        const recipes = await Recipe.find();
+        res.send(recipes);
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
 });
 
-app.put("/todos/:todoId/", async (request, response) => {
-  const { todoId } = request.params;
-  let updateColumn = "";
-  const requestBody = request.body;
-  switch (true) {
-    case requestBody.status !== undefined:
-      updateColumn = "Status";
-      break;
-    case requestBody.priority !== undefined:
-      updateColumn = "Priority";
-      break;
-    case requestBody.todo !== undefined:
-      updateColumn = "Todo";
-      break;
-  }
-  const previousTodoQuery = `
-    SELECT
-      *
-    FROM
-      todo
-    WHERE 
-      id = ${todoId};`;
-  const previousTodo = await database.get(previousTodoQuery);
-
-  const {
-    todo = previousTodo.todo,
-    priority = previousTodo.priority,
-    status = previousTodo.status,
-  } = request.body;
-
-  const updateTodoQuery = `
-    UPDATE
-      todo
-    SET
-      todo='${todo}',
-      priority='${priority}',
-      status='${status}'
-    WHERE
-      id = ${todoId};`;
-
-  await database.run(updateTodoQuery);
-  response.send(`${updateColumn} Updated`);
+app.post('/recipes', async (req, res) => {
+    try {
+        const recipe = new Recipe(req.body);
+        await recipe.save();
+        res.status(201).send(recipe);
+    } catch (error) {
+        res.status(400).send({ error: error.message });
+    }
 });
 
-app.delete("/todos/:todoId/", async (request, response) => {
-  const { todoId } = request.params;
-  const deleteTodosQuery = `
-  DELETE FROM
-    todo
-  WHERE
-    id = ${todoId};`;
-
-  await database.run(deleteTodosQuery);
-  response.send("Todo Deleted");
+app.put('/recipes/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const recipe = await Recipe.findByIdAndUpdate(id, req.body, { new: true });
+        if (!recipe) {
+            throw new Error('Recipe not found');
+        }
+        res.send(recipe);
+    } catch (error) {
+        res.status(400).send({ error: error.message });
+    }
 });
 
-module.exports = app;
+app.delete('/recipes/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const recipe = await Recipe.findByIdAndDelete(id);
+        if (!recipe) {
+            throw new Error('Recipe not found');
+        }
+        res.send(recipe);
+    } catch (error) {
+        res.status(400).send({ error: error.message });
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
